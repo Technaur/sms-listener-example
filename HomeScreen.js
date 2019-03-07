b66b50b19d557afba2f5781912cbcde59da3b68e
Franco Schwim <francoschwim@hotmail.com>
2019-03-07 19:38:03 +0200

    Added react-native-elements, fixed some sms listening stuff

---
+++ HomeScreen.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react"
+import { Button, Text } from "react-native-elements"
+import { View, StyleSheet } from "react-native"
+import SmsRetriever from "react-native-sms-retriever"
+
+export class HomeScreen extends Component {
+  state = {
+    message: null,
+    listening: false
+  }
+
+  _onSmsListenerPressed = async () => {
+    try {
+      const registered = await SmsRetriever.startSmsRetriever()
+      console.log(`Registered: ${registered}`)
+      if (registered) {
+        this.setState({ listening: registered, message: null })
+        SmsRetriever.addSmsListener(event => {
+          console.log(`Response: ${event.message}`)
+          this.setState({ message: event.message, listening: false }, () => {
+            SmsRetriever.removeSmsListener()
+          })
+        })
+      }
+    } catch (error) {
+      console.log(JSON.stringify(error))
+    }
+  }
+
+  render() {
+    const { message, listening } = this.state
+    return (
+      <View style={styles.container}>
+        <View style={{ alignItems: "center" }}>
+          <Text h3> SMS Listener Example </Text>
+          <Button
+            raised
+            buttonStyle={{ width: 300 }}
+            loading={listening}
+            title="Register SMS Listening"
+            onPress={() => {
+              this._onSmsListenerPressed()
+            }}
+          />
+        </View>
+
+        <Text h4>{`Message Received: \n\n${message}`}</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    backgroundColor: "white"
+  }
+})
+
+export default HomeScreen
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-.\keytool -list -v -keystore 'C:\Users\<user>\.android\debug.keystore'     ---Get alias name
-.\keytool -alias androiddebugkey -exportcert -keystore 'C:\Users\<user>\.android\debug.keystore'
+## Generate the 11 character hash
+
+---
+
+1.  <u>**Build your keytool command. (I ran mine in bash, powershell might behave differently)**</u>
+
+    Just <b>replace</b> `username` and `com.sms_listener` with your own details.
+    The default `-alias` for the debug keystore will always be **`androiddebugkey`**
+
+    ```
+    keytool -exportcert -alias androiddebugkey -keystore 'C:\Users\<<<username>>>\.android\debug.keystore' | xxd -p | tr -d "[:space:]" | echo -n <<<com.sms_listener>>> `cat` | sha256sum | tr -d "[:space:]-" | xxd -r -p | base64 | cut -c1-11
+    ```
+
+    If you are using the debug keystore like me, it will ask for a password after you run the above command.
+    The default password is: <b>`android`</b>
+
+    A string will then be generated, something like this: <b>`gaMzAZ+vMRr`</b>
+
+2.  <u>**In the extended controls for Android Emulator**</u>
+
+    Phone --> SMS Message
+
+    ```
+    <#> Your Example app code is: 123ABC78 /gaMzAZ+vMRr
+    ```
+
+    Of course replacing the 11 hash characters at the end with your own :)
+
+---
+
+### Bingo, it worked
